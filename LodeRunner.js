a64dc533c9be35afb8651a0623b933488dd6ccf5
Paulico <d.paulico@campus.fct.unl.pt>
2020-05-29 14:53:42 +0100

    height hole lose

--- LodeRunner.js
+++ LodeRunner.js
@@ -274,7 +274,7 @@ class Hero extends ActiveActor {
 	}
 
 	animation() {
-		if(this.isInHole(this.x,this.y)){
+		if(this.isInHole(this.x,this.y) && y == WORLD_HEIGHT){
 			control.resetLevel();
 			return;
 		}
